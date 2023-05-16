ce211c16814f0c179535fcccc21d4465992aa9fa
Nick <nicknanos7@gmail.com>
2023-05-16 16:57:07 +0300

    Add custom validity

--- index.js
+++ index.js
@@ -10,16 +10,19 @@ var card_cvc = document.querySelector('.cvc');
 //input
 const inputs = document.querySelectorAll('form input');
 
+//errors
+const error_number =document.querySelector('.error[for=number]');
+const error_name =document.querySelector('.error[for=name]');
+const error_date =document.querySelector('.error[for=date]');
+const error_cvc =document.querySelector('.error[for=cvc]');
+
+
 const button  = document.querySelector('button[type=submit]');
 
 const dialog = document.querySelector('.end-state');
 const modal = document.querySelector('.modal');
 
-/* function empty(e) {
-    let container = `card_${e.target.id}`;
-    //console.log(container);
-    window[container].textContent = ''
-} */
+let valid = false;
 
 function update(e) {
     let container = `card_${e.target.id}`;
@@ -27,18 +30,92 @@ function update(e) {
     window[container].textContent = e.target.value;
 }
 
+function isEmpty(e) {
+    console.log(e.target.value);
+    if(e.target.value==''){
+        return true;
+    }else return false;
+}
+
+function checkDate(e){
+    if(e.target.checkValidity()){
+        let value =parseInt(e.target.value);
+        if(e.target.id=='month'){
+            if(value<1 || value>12){
+                error_date.textContent = 'Invalid Date';
+                return true;
+            }
+        }else {
+            if(value<2010 || value>2030){
+                error_date.textContent = 'Invalid Date';  
+                return true;
+            }
+        }
+        return false;
+    }
+}
+
+function validate(e) {
+    e.preventDefault();
+    e.target.classList.add('touched');
+    let errorBox = getError(e);
+    if(isEmpty(e)){
+        errorBox.textContent = 'Cant be blank'
+        valid = false;
+    }else if(errorBox){
+        if(e.target.checkValidity()){
+            errorBox.innerHTML = '&#8203';
+            valid = true;
+        }else {
+            errorBox.textContent = "Invalid Input";
+            valid = false;
+        }
+    }
+}
+
+
+function getError(e) {
+    let error = e.target.id;
+    switch(error){
+        case 'number':
+            return error_number;
+        case 'name':
+            return error_name;
+        case 'month':
+            if(checkDate(e)) break;
+            return error_date;
+        case 'year':
+            if(checkDate(e)) break;
+            return error_date;
+        case 'cvc':
+            return error_cvc;
+    }
+}
+
 inputs.forEach((input)=>{
     //input.addEventListener('click',empty,{once: true});
-    input.addEventListener('input',update);
+    input.addEventListener('input',(e)=>{
+        update(e);
+        validate(e);
+    });
 })
 
 button.addEventListener('click',(e)=>{
-    e.preventDefault;
-    dialog.showModal();
-    dialog.classList.toggle('displayed');
+    inputs.forEach((input)=>{
+        input.addEventListener('input',validate)
+    })
+    if(valid){
+        e.preventDefault();
+        dialog.showModal();
+        dialog.classList.toggle('displayed');
+    }else {
+        e.preventDefault();
+        alert("There's been one or more errors on your information")
+    }
 })
 
 modal.addEventListener('click',()=>{
     dialog.classList.toggle('displayed');
     dialog.close();
+    location.reload();
 })
